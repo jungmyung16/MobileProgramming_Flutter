d1286ff21d3404323613e5e602c712ed3d3eddb6
jungmyung <jungmyung30830@gmail.com>
2024-07-14 10:13:37 +0900

    상태 관리하기

--- lib/day12/ch18/ch18_2.dart
+++ lib/day12/ch18/ch18_2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_20195238/day12/ch18/ch18_1.dart';
 
 void main() {
   runApp(ParentWidget());
@@ -26,5 +27,101 @@ class ParentWidgetState extends State<ParentWidget> {
         favorited = true;
       }
     });
+  }
+
+  void getChildData() {
+    ChildWidgetState? childState = childKey.currentState;
+    setState(() {
+      childCount = childState?.childCount ?? 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('20195238 장정명'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Text('I am Parent, child count : $childCount'),
+                ),
+                ElevatedButton(onPressed: getChildData, child: Text('get child data')),
+              ],
+            ),
+            ChildWidget(key: childKey),
+            IconWidget(),
+            ContentWidget()
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class ChildWidget extends StatefulWidget {
+  ChildWidget({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return ChildWidgetState();
+  }
+}
+
+class ChildWidgetState extends State<ChildWidget> {
+  int childCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: Text('I am Child, $childCount'),
+        ),
+        ElevatedButton(
+          child: Text('increment'),
+          onPressed: () {
+            setState(() {
+              childCount++;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class IconWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ParentWidgetState? state = context.findAncestorStateOfType<ParentWidgetState>();
+    return Center(
+      child: IconButton(
+        icon: ((state?.favorited ?? false) ? Icon(Icons.favorite) : Icon(Icons.favorite_border)),
+        color: Colors.red,
+        iconSize: 200,
+        onPressed: state?.toggleFavorite,
+      ),
+    );
+  }
+}
+
+class ContentWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ParentWidgetState? state = context.findAncestorStateOfType<ParentWidgetState>();
+    return Center(
+      child: Text(
+        'favoriteCount : ${state?.favoriteCount}',
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
